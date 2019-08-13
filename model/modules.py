4605bf5282abf6f1d578879aa11ec01af96e6901
Htoo Pyae <htoopyae5@gmail.com>
2019-08-14 01:26:06 +0630

    core model modules implemented

--- model/modules.py
+++ model/modules.py
@@ -1,129 +1,98 @@
-import keras.backend as K
-import keras.initializers as k_init
-from keras.layers import (Conv1D, Dense, Activation, MaxPooling1D, 
-                          Add, Concatenate, Bidirectional, GRU, Dropout,
-                          BatchNormalization, Lambda, Dot, Multiply)
+import tensorflow as tf
+from keras.layers import (Dense, Dropout, Conv1D, BatchNormalization, Bidirectional,
+                          MaxPooling1D, GRUCell)
 
-def Pre_Net(input_data):
-  """
-  Pre-Net
+
+def prenet(inputs, is_training, layer_sizes, scope=None):
+  """ Pre-Net
 
   FC-256-ReLU -> Dropout(0.5) -> FC-128-ReLU -> Dropout(0.5)
   """
-  prenet = Dense(256)(input_data)
-  prenet = Activation('relu')(prenet)
-  prenet = Dropout(0.5)(prenet)
-  prenet = Dense(128)(prenet)
-  prenet = Activation('relu')(prenet)
-  prenet = Dropout(0.5)(prenet)
+  prenet = inputs
+  drop_rate = 0.5 if is_training else 0.0
+
+  with tf.variable_scope(scope or 'prenet'):
+    prenet = Dense(256, activation='relu')(prenet)
+    prenet = Dropout(drop_rate, training=is_training)(prenet)
+    prenet = Dense(128, activation='relu')(prenet)
+    prenet = Dropout(drop_rate, training=is_training)(prenet)
 
   return prenet
 
 
-def Conv1D_Bank(K_, input_data):
-  """
-  1-D Convolution Bank
+def conv1d(inputs, kernel_size, filters, activation, is_training, scope):
+  """ Conv1d with batch normalization """
+  with tf.variable_scope(scope):
+    conv1d = Conv1D(filters=filters, kernel_size=kernel_size, activation=activation, padding='same')(inputs)
+    conv1d = BatchNormalization(training=is_training)(conv1d)
+
+    return conv1d
+
+
+def conv1d_bank(inputs, K, is_training):
+  """ 1-D Convolution Bank
 
   K-layers of Conv1D filters to model Unigrams, Bigrams and so on
   """
-  conv = Conv1D(filters=128, kernel_size=1, strides=1, padding='same')(input_data)
-  conv = BatchNormalization()(conv)
-  conv = Activation('relu')(conv)
-
-  for k_ in range(2, K_+1):
-    conv = Conv1D(filters=128, kernel_size=k_, strides=1, padding='same')(conv)
-    conv = BatchNormalization()(conv)
-    conv = Activation('relu')(conv)
-
-  return conv
+  with tf.variable_scope('conv1d_bank'):
+    bank = tf.concat(
+      [conv1d(inputs, k, 128, tf.nn.relu, is_training, 'conv1d_%d' % k) for k in range(1, K+1)],
+      axis=-1
+    )
+    return bank
 
 
-def Highway_Net(highway_input, nb_layers, activation = 'relu', bias = -3):
-  """
-  Highway Network
-  """
-  dim = K.int_shape(highway_input)[-1]
-  initial_bias = k_init.Constant(bias)
+def highwaynet(highway_input, nb_layers, depth):
+  """ HighwayNet """
+  for i in range(nb_layers):
+    with tf.variable_scope('highway_%d' % (i+1)):
+      H = Dense(depth, activation='relu', name='H')(highway_input)
+      T = Dense(depth, activation='sigmoid', name='T', bias_initializer=tf.constant_initializer(-1.0))(highway_input)
 
-  for n in range(nb_layers):
-    H = Dense(units=dim, bias_initializer=initial_bias)(highway_input)
-    H = Activation('sigmoid')(H)
-    carry_gate = Lambda(lambda x: 1.0 - x, output_shape=(dim,))(H)
-    transform_gate = Dense(units=dim)(highway_input)
-    transform_gate = Activation(activation)(transform_gate)
-    transformed = Multiply()([H, transform_gate])
-    carried = Multiply()([carry_gate, highway_input])
-    highway_output = Add()([transformed, carried])
+      highway_output = H * T + highway_input * (1.0 - T)
 
   return highway_output
 
 
-def CBHG(input_data, K_CBHG, for_encoder = True):
+def cbhg(inputs, is_training, scope, K, projections):
+  """ CBHG module (Conv1D Bank, Highway Net, Bidirectional GRU)
+
+  a powerful module for extracting representations from sequences
   """
-  CBHG module (Conv1D Bank, Highway Net, Bidirectional GRU)
-  """
-  # Conv1D Bank
-  conv1d_bank = Conv1D_Bank(K_CBHG, input_data)
-  # Max Pooling 
-  conv1d_bank = MaxPooling1D(pool_size=2, strides=1, padding='same')(conv1d_bank)
-  # Conv1D
-  conv1d_bank = Conv1D(filters=128, kernel_size=3, strides=1, padding='same')(conv1d_bank)
-  # Batch Normalization
-  conv1d_bank = BatchNormalization()(conv1d_bank)
-  # Residual Connection
-  residual = Add()([conv1d_bank, input_data])
-  # Highway Net
-  highway_net = Highway_Net(residual, 4, activation='relu')
+  with tf.variable_scope(scope):
+    # Conv1D bank 
+    #   encoder :  K=16, conv-k-128-ReLU
+    #   post    :  K=8,  conv-k-128-ReLU
+    conv_outputs = conv1d_bank(inputs, K, is_training)
 
-  if for_encoder:
-    # for encoder
-    CBHG = Bidirectional(GRU(128, return_sequences=True))(highway_net)
-  else:
-    # for decoder
-    CBHG = Bidirectional(GRU(128))(highway_net)
+    # Maxpooling 
+    #   stride=1, width=2
+    maxpool_output = MaxPooling1D(pool_size=2, strides=1, padding='same')(conv_outputs)
 
-  return CBHG
+    # Two projections layers
+    #   encoder :  conv-3-128-ReLU → conv-3-128-Linear
+    #   post    :  conv-3-256-ReLU → conv-3-80-Linear
+    proj1_output = conv1d(maxpool_output, 3, projections[0], tf.nn.relu, is_training, 'proj_1')
+    proj2_output = conv1d(proj1_output, 3, projections[1], None, is_training, 'proj_2')
 
+    # Residual connection
+    highway_input = proj2_output + inputs
 
-def Attention_RNN():
-  """
-  Attention RNN
+    # 4-layer HighwayNet
+    highway_output = highwaynet(highway_input, 4, 128)
 
-  1-layer GRU with 256 units
-  """
-  return GRU(256)
+    # Bidirectional GRU 128 cells
+    outputs = Bidirectional(GRUCell(128), backward_layer=GRUCell(128))(highway_output)
+
+    return outputs
 
 
-def Decoder_RNN(input_data):
-  """
-  Decoder RNN
-
-  2-layer GRU with vertical residual connections
-  ***
-  have to define return_sequences=True for the first GRU layer -> an output will be returned for each timestep
-  if don't, the 1st GRU returns only one output for the entire input sequence while the 2nd GRU expects a sequence as input
-  ***
-  """
-  rnn1 = GRU(256, return_sequences=True)(input_data)
-
-  inp2 = Add()([input_data, rnn1])
-  rnn2 = GRU(256)(inp2)
-
-  decoder_rnn = Add()([inp2, rnn2])
-
-  return decoder_rnn
+def encoder_cbhg(inputs, is_training):
+  """ Encoder CBHG """
+  input_channels = inputs.get_shape()[2]
+  return cbhg(inputs, is_training, scope='encoder_cbhg', K=16, projections=[128, input_channels])
 
 
-def Attention_Context(encoder_output, attention_rnn_output):
-  """
-  Attention Mechanism
-
-  (CBHG_encoder_ouput + RNN_output) -> tanh -> relu -> softmax -> * CBHG_encoder_output
-  """
-  attention_input = Concatenate(axis=-1)([encoder_output, attention_rnn_output])
-  e = Dense(10, activation='tanh')(attention_input)
-  energies = Dense(1, activation='relu')(e)
-  attention_weights = Activation('softmax')(energies)
-  context = Dot(axes=1)([attention_weights, encoder_output])
-
-  return context
+def post_cbhg(inputs, input_dim, is_training):
+  """ Post-processing net CBHG """
+  return cbhg(inputs, is_training, scope='post_cbhg', K=8, projections=[256, input_dim])
